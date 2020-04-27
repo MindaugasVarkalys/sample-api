86809cb069b22a92584ae542e04534a42cc9c43b
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2020-04-27 18:22:03 +0300

    Remove printing to console

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require('uWebSockets.js').App()
 .any('/', (res, req) => {
-
-    console.log("GOT REQUEST!");
     res.end('Hello world!');
-
 }).listen(8000, (listenSocket) => {
     if (listenSocket) {
         console.log('Listening to port 8000');
